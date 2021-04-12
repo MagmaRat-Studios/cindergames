5914154a66bfb777ec7ae4e4785e6ae13b8b7631
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 21:37:31 -0400

    Add files via upload

---
+++ src/_common/escape-stack/escape-stack.service.ts
@@ -0,0 +1,37 @@
+import { arrayRemove } from '../../utils/array';
+
+export type EscapeStackCallback = () => void;
+
+export class EscapeStack {
+	static stack: EscapeStackCallback[] = [];
+	private static initialized = false;
+
+	static register(cb: EscapeStackCallback) {
+		this.stack.push(cb);
+		this.init();
+	}
+
+	static deregister(cb: EscapeStackCallback) {
+		arrayRemove(this.stack, i => i === cb);
+	}
+
+	private static handle(e: KeyboardEvent) {
+		if (e.key !== 'Escape') {
+			return;
+		}
+
+		const top = this.stack[this.stack.length - 1];
+		if (top) {
+			top();
+		}
+	}
+
+	private static init() {
+		if (this.initialized) {
+			return;
+		}
+
+		document.addEventListener('keydown', e => this.handle(e));
+		this.initialized = true;
+	}
+}
