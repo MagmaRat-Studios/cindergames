2bb6100f7b1ea20659c70610d75c62f1be3257e6
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 21:06:30 -0400

    Add files via upload

---
+++ src/app/components/tag/list/list.vue
@@ -0,0 +1,33 @@
+<script lang="ts" src="./list"></script>
+
+<template>
+	<app-scroll-scroller horizontal thin>
+		<div class="-list">
+			<div v-for="tag of tags" :key="tag.id" class="-list-item">
+				<app-tag-thumbnail class="-list-thumb" :tag="tag.id" :event-cat="eventCat" />
+			</div>
+		</div>
+	</app-scroll-scroller>
+</template>
+
+<style lang="stylus" scoped>
+@import '~styles/variables'
+
+.-list
+	white-space: nowrap
+	text-align: center
+	padding-top: ($grid-gutter-width / 2)
+	padding-bottom: ($grid-gutter-width / 2)
+
+	@media $media-xs
+		padding-top: ($grid-gutter-width-xs / 2)
+		padding-bottom: ($grid-gutter-width-xs / 2)
+
+	&-item
+		display: inline-block
+		width: 130px
+		margin: 0 10px
+
+	&-thumb
+		margin-bottom: 0 !important
+</style>
