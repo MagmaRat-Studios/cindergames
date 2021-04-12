c960fe9a72b0947bed38382d98f600c20c8bc8db
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 21:24:36 -0400

    Add files via upload

---
+++ src/_common/shell/shell.vue
@@ -0,0 +1,12 @@
+<template>
+	<app-scroll-inview-parent>
+		<slot />
+
+		<app-theme />
+		<app-loading-bar />
+		<app-growls />
+		<app-modals />
+	</app-scroll-inview-parent>
+</template>
+
+<script lang="ts" src="./shell"></script>
