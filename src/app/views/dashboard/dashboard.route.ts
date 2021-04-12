cdafdb37c35814bbeec3ce11e5abeb55582b98b5
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 20:45:14 -0400

    Add files via upload

---
+++ src/app/views/dashboard/dashboard.route.ts
@@ -0,0 +1,23 @@
+import { RouteConfig } from 'vue-router';
+import { routeDashAccount } from './account/account.route';
+import { routeDashAccountMobileNav } from './account/mobile-nav.route';
+import { routeDashAnalytics } from './analytics/analytics.route';
+import { routeDashCommunities } from './communities/communities.route';
+import { routeDashGames } from './games/games.route';
+import { routeDashLinking } from './linking/linking.route';
+import { routeDashStickers } from './stickers/stickers.route';
+
+export const routeDash: RouteConfig = {
+	name: 'dashboard',
+	path: '/dashboard',
+	component: () => import(/* webpackChunkName: "routeDash" */ './dashboard.vue'),
+	children: [
+		routeDashAccount,
+		routeDashAccountMobileNav,
+		routeDashGames,
+		routeDashCommunities,
+		routeDashAnalytics,
+		routeDashLinking,
+		routeDashStickers,
+	],
+};
