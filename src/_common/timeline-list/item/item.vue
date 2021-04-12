f79b4cade408976ea1ff55e07b36f668fad3e390
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 21:22:14 -0400

    Add files via upload

---
+++ src/_common/timeline-list/item/item.vue
@@ -0,0 +1,25 @@
+<template>
+	<div
+		class="timeline-list-item"
+		:class="{
+			new: isNew,
+			active: isActive,
+			'is-thread': isThread,
+			'is-last': isLast,
+		}"
+	>
+		<div class="timeline-list-item-connector" />
+
+		<div v-if="!!$slots.bubble" class="timeline-list-item-bubble">
+			<div class="timeline-list-item-bubble-inner">
+				<slot name="bubble" />
+			</div>
+		</div>
+
+		<div class="timeline-list-item-main">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script lang="ts" src="./item"></script>
