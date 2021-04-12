06a1664aecdf7565571f8b092413ae6510da4f52
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 21:39:36 -0400

    Create content-owner.ts

---
+++ src/_common/content/content-owner.ts
@@ -0,0 +1,17 @@
+import { ContentContext, ContextCapabilities } from './content-context';
+import { ContentDocument } from './content-document';
+import { ContentRules } from './content-editor/content-rules';
+import { ContentHydrator } from './content-hydrator';
+
+export interface ContentOwner {
+	getHydrator(): ContentHydrator;
+	getCapabilities(): ContextCapabilities;
+	getContext(): ContentContext;
+
+	getContent(): ContentDocument | null;
+	setContent(content: ContentDocument): void;
+
+	getModelId(): Promise<number>;
+
+	getContentRules(): ContentRules;
+}
