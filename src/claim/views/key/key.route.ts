70c804eba8b0e08c65a6626a609e4869ad207c25
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 20:26:58 -0400

    Add files via upload

---
+++ src/claim/views/key/key.route.ts
@@ -0,0 +1,7 @@
+import { RouteConfig } from 'vue-router';
+
+export const routeKey: RouteConfig = {
+	name: 'key',
+	path: '/claim/:accessKey([a-zA-Z0-9]+)/:bundleGameId(\\d+)?',
+	component: () => import(/* webpackChunkName: "routeKey" */ './key.vue'),
+};
