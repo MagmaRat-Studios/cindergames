a51eb5ccf3961214c6bef17f28bf97101bc311e2
DeSynk <69215033+DeSynkDEV@users.noreply.github.com>
2021-04-11 20:13:18 -0400

    Add files via upload

---
+++ typings/environment.d.ts
@@ -0,0 +1,13 @@
+declare const GJ_SECTION: string;
+declare const GJ_ENVIRONMENT: 'development' | 'production';
+declare const GJ_BUILD_TYPE: 'development' | 'production';
+declare const GJ_IS_CLIENT: boolean;
+declare const GJ_IS_SSR: boolean;
+declare const GJ_VERSION: string;
+declare const GJ_MANIFEST_URL: string;
+declare const GJ_WITH_UPDATER: boolean;
+declare const GJ_IS_WATCHING: boolean;
+declare const GJ_TUNNELS: {
+	backend?: string;
+	frontend?: string;
+};
